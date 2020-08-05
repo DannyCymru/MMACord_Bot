78eeae162ab1c51e4f30274e636171e881df2c3c
Danny <danny@erebus.cymru>
2020-08-05 07:53:46 +0100

    Finished scraping for links on the webpage

--- MMACord_Bot/src/commands/decision.rs
+++ MMACord_Bot/src/commands/decision.rs
@@ -1,21 +1,26 @@
+extern crate reqwest;
+extern crate select;
+
 use serenity::prelude::*;
 use serenity::model::prelude::*;
 use serenity::framework::standard::{
     CommandResult,
     macros::command,
 };
+use select::document::Document;
+use select::predicate::{Attr, Class, Name, Predicate};
 
 #[command]
 fn decision(ctx: &mut Context, msg: &Message) -> CommandResult {
 	
 	let name_vec: Vec<String> = names((*msg.content).to_string());
 
-	let fighter_1 = name_vec[0].to_owned() + " " + &name_vec[1];
+	let fighter_1 = name_vec[0].to_owned() + "+" + &name_vec[1];
 
-	let fighter_2 = name_vec[3].to_owned() + " " + &name_vec[4];
+	let fighter_2 = name_vec[3].to_owned() + "+" + &name_vec[4];
 
-	
-	println!("{} {}", fighter_1, fighter_2);
+	fighter_search(fighter_1, fighter_2);
+
     let _ = msg.channel_id.say(&ctx.http, "Decision");
 
     Ok(())
@@ -40,4 +45,23 @@ fn names(mut content: String) -> std::vec::Vec<String> {
 
 fn fighter_search(fighter_1: String, fighter_2: String){
 
+	//Sets up http client
+	let client = reqwest::blocking::Client::new();
+
+	//Base search url
+	let origin_url = "http://www.mmadecisions.com/search?s=";
+	
+	//Search url for the first fighter user specified
+	let fighter_1_se = origin_url.to_owned() + &fighter_1;
+	let res = client.get(&fighter_1_se).send().unwrap();
+
+	//Checks the page status eg 200 or 404
+	println!("Status for {}: {}", &fighter_1_se, res.status());
+
+	//Webpage scrape
+	Document::from_read(res)
+		.unwrap()
+		.find(Name("a"))
+		.filter_map(|n| n.attr("href"))
+		.for_each(|x| println!("{}", x));
 }
